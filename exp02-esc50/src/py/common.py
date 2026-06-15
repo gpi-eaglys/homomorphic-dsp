faba45beffae55798d0ac630ea633902973888d6
Gabor Pinter <g_pinter@eaglys.co.jp>
2026-06-15 19:54:56 +0900

    Fix build dir for exp02

--- exp02-esc50/src/py/common.py
+++ exp02-esc50/src/py/common.py
@@ -10,8 +10,8 @@ from torch.utils.data import Dataset
 
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 EXP_DIR    = os.path.abspath(os.path.join(SCRIPT_DIR, "../.."))
-BLD_DIR    = os.path.join(EXP_DIR, "build")
 REPO_DIR   = os.path.abspath(os.path.join(EXP_DIR, ".."))
+BLD_DIR    = os.path.join(REPO_DIR, "build")
 ESC50_ROOT       = os.path.join(REPO_DIR, "assets/esc-50/ESC-50-master")
 ESC50_FPATH_META = os.path.join(ESC50_ROOT, "meta/esc50.csv")
 ESC50_AUDIO_DIR = os.path.join(ESC50_ROOT, "audio")
